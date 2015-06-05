62ad21259ba971721bb615c0e2ae2ea2ed13cd94
Kuteken <kuteken@gmail.com>
2015-06-06 03:37:47 +0700

    Add Showing Some Text.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,26 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_time_layer;
 
 static void main_window_load(Window *window) {
-  
+  // Create time text layer
+  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "0:00");
+
+  // Improve the layout to be more like a watch interface
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add is as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-  
+    // Destroy TextLayer
+    text_layer_destroy(s_time_layer);
 }
 
 static void init() {
