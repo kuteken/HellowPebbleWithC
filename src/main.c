394aab7ad15647c037ce26855df17587afbe24ac
Kuteken <kuteken@gmail.com>
2015-06-06 17:46:47 +0700

    Add Telling the Time

--- src/main.c
+++ src/main.c
@@ -3,6 +3,27 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  if (clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
 static void main_window_load(Window *window) {
   // Create time text layer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
@@ -16,11 +37,18 @@ static void main_window_load(Window *window) {
 
   // Add is as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  // Make sure the time is displayed from the start
+  update_time();
 }
 
 static void main_window_unload(Window *window) {
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void init() {
@@ -35,6 +63,9 @@ static void init() {
   
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
